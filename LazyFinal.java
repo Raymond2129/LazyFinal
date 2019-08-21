80ddd6076a39a1a3f287ba18fcd0c0c6f9e08ccb
Ray Ray <raymond2129@gmail.com>
2019-08-20 19:04:58 -0600

    added override commands in lazyfinal file

--- LazyFinal.java
+++ LazyFinal.java
@@ -11,6 +11,119 @@ public final class LazyFinal<T> {
     private T value;
     private Supplier<T> populator;
 
+    @Override
+    public String toString() {
+    return this.get().toString();
+    }
+    
+    @Override
+    public int hashCode() {
+    return this.get().hashCode();
+    }
+
+    @Override
+    public boolean equals() {
+    return this.get().equals();
+    }
+    
+
+    @Override
+	//implement hashCode() method
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((department == null) ? 0 : department.hashCode());
+		result = prime * result + ((firstname == null) ? 0 : firstname.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		return result;
+	}
+
+	@Override
+	//implement equals() method
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (department == null) {
+			if (other.department != null)
+				return false;
+		} else if (!department.equals(other.department))
+			return false;
+		if (firstname == null) {
+			if (other.firstname != null)
+				return false;
+		} else if (!firstname.equals(other.firstname))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		return true;
+	}
+
+	@Override
+	public boolean equals(Object o) {
+		if(o == null)
+		{
+			return false;
+		}
+		if (o == this)
+		{
+           return true;
+        }
+		if (getClass() != o.getClass())
+		{
+	        return false;
+		}
+		Employee e = (Employee) o;
+		return (this.getId() == e.getId());
+	}
+	
+	@Override
+	public int hashCode() 
+	{
+		final int PRIME = 31;
+	    int result = 1;
+	    result = PRIME * result + getId();
+	    return result;
+	}
+	
+	
+	@Override
+	public int hashCode() 
+	{
+		final int PRIME = 31;
+        return new HashCodeBuilder(getId()%2==0?getId()+1:getId(), PRIME).
+            toHashCode();
+    }
+
+	@Override
+    public boolean equals(Object o) {
+        if (o == null)
+            return false;
+        if (o == this)
+            return true;
+        if (o.getClass() != getClass())
+            return false;
+        Employee e = (Employee) o;
+        return new EqualsBuilder().
+            append(getId(), e.getId()).
+            isEquals();
+    }
+	
+	
+
     /**
      * Constructs a lazy final using the given populator function.
      * <p>
